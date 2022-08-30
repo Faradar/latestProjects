5daa914e176aa6f24717f937d4eca82824f3aec2
Faradar <tomi_compuamste@hotmail.com>
2022-08-30 18:13:15 -0300

    Agregando el boton para volver al menu

--- piedra papel o tijera/pptijera.js
+++ piedra papel o tijera/pptijera.js
@@ -4,50 +4,61 @@ Que tenga imagenes clickeables.
 Que tenga una pantalla de seleccion de modo con botones que luego te lleven al juego
 */
 
-let gameMode = parseInt(prompt("Elige en que modo quieres jugar: 1. Singleplayer 2. Multiplayer")),
-  player1 = 0,
+let player1 = 0,
   player2 = 0,
   score1 = 0,
   score2 = 0,
+  single = false,
+  multi = false,
   parrafo0 = document.getElementById("par0"),
-  parrafo1 = document.getElementById("par1"),
-  parrafo2 = document.getElementById("par2");
-main();
-function main() {
-  if (gameMode == 1) {
-    parrafo0.innerHTML = "<h2>Singleplayer</h2><p>Elige piedra, papel o tijera!</p>";
-    document.getElementById("Papel").onclick = function() {
-      player1 = 2;
-      player2 = aleatorio(1,3);
-      jugar (player1, player2);
-    };
-    document.getElementById("Piedra").onclick = function() {
-      player1 = 1;
-      player2 = aleatorio(1,3);
-      jugar (player1, player2);
-    };
-    document.getElementById("Tijera").onclick = function() {
-      player1 = 3;
-      player2 = aleatorio(1,3);
-      jugar (player1, player2);
-    };
-  } else if (gameMode == 2){
-      parrafo0.innerHTML = "<h2>Multiplayer</h2><p>Jugador 1: Elige piedra, papel o tijera!</p>";
-      document.getElementById("Papel").onclick = function() {
-        player1 = 2;
-        elegir();
-      };
-      document.getElementById("Piedra").onclick = function() {
-        player1 = 1;
-        elegir();
-      };
-      document.getElementById("Tijera").onclick = function() {
-        player1 = 3;
-        elegir();
-      };
-  } else {
-      document.write("Error, escribe una de las opciones dadas por favor.");
-  }
+  parrafo1 = 0,
+  parrafo2 = 0;
+function setupSingle() {
+  images();
+  parrafos();
+  goBack();
+  singleMode();
+}
+function setupMulti() {
+  images();
+  parrafos();
+  goBack();
+  multiMode();
+}
+function singleMode() {
+  single = true;
+  parrafo0.innerHTML = "<h2>Singleplayer</h2><p>Elige piedra, papel o tijera!</p>";
+  document.getElementById("Papel").onclick = function() {
+    player1 = 2;
+    player2 = aleatorio(1,3);
+    jugar (player1, player2);
+  };
+  document.getElementById("Piedra").onclick = function() {
+    player1 = 1;
+    player2 = aleatorio(1,3);
+    jugar (player1, player2);
+  };
+  document.getElementById("Tijera").onclick = function() {
+    player1 = 3;
+    player2 = aleatorio(1,3);
+    jugar (player1, player2);
+  };
+} 
+function multiMode() {
+  multi = true;
+  parrafo0.innerHTML = "<h2>Multiplayer</h2><p>Jugador 1: Elige piedra, papel o tijera!</p>";
+  document.getElementById("Papel").onclick = function() {
+    player1 = 2;
+    elegir();
+  };
+  document.getElementById("Piedra").onclick = function() {
+    player1 = 1;
+    elegir();
+  };
+  document.getElementById("Tijera").onclick = function() {
+    player1 = 3;
+    elegir();
+  };
 }
 function aleatorio(min, max){
 	let resultado;
@@ -77,8 +88,11 @@ function jugar(j1, j2) {
     score2++;
     parrafo2.innerHTML = "Score Jugador 1: " + score1 + "<br>Score Jugador 2: " + score2;
   }
-  if (gameMode == 2) {
-    main();
+  if (single) {
+    singleMode();
+  }
+  else if (multi) {
+    multiMode();
   }
 }
 function elegir() {
@@ -95,4 +109,48 @@ function elegir() {
     player2 = 3;
     jugar (player1, player2);
   };
+} 
+function images(){
+  const imgPapel = document.createElement("img"),
+    imgPiedra = document.createElement("img"),
+    imgTijera = document.createElement("img");
+  imgPapel.src = "img/papel.png"; imgPapel.id = "Papel";
+  imgPiedra.src = "img/piedra.png"; imgPiedra.id = "Piedra";
+  imgTijera.src = "img/tijera.png"; imgTijera.id = "Tijera";
+  document.getElementById("body").appendChild(imgPapel);
+  document.getElementById("body").appendChild(imgPiedra);
+  document.getElementById("body").appendChild(imgTijera);
+}
+function parrafos(){
+  parrafo1 = document.createElement("p"),
+  parrafo2 = document.createElement("p");
+  parrafo1.id = "par1" ; parrafo2.id = "par2";
+  document.getElementById("body").appendChild(parrafo1);
+  document.getElementById("body").appendChild(parrafo2);
+}
+function goBack(){
+  const refresh = document.createElement("button");
+  document.getElementById("body").appendChild(refresh);
+  refresh.innerHTML = "Menu";
+  refresh.onclick = function () {
+    score1 = 0;
+    score2 = 0;
+    single = false;
+    multi = false;
+    parrafo0.innerHTML = "Elige en que modo quieres jugar:<br><br>";
+    parrafo1.remove();
+    parrafo2.remove();
+    refresh.remove();
+    Papel.remove();
+    Piedra.remove();
+    Tijera.remove();
+    const singlePlayer = document.createElement("button"),
+      multiPlayer = document.createElement("button");
+    document.getElementById("par0").appendChild(singlePlayer);
+    document.getElementById("par0").appendChild(multiPlayer);
+    singlePlayer.innerHTML = "Singleplayer";
+    multiPlayer.innerHTML = "Multiplayer";
+    singlePlayer.onclick = setupSingle;
+    multiPlayer.onclick = setupMulti;
+  }
 }
