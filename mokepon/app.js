00b3b781a6a92ed27dfee65aace0ff19a8ea03b0
Faradar <tomi_compuamste@hotmail.com>
2022-09-06 22:30:27 -0300

    ligero cambio a mokepon

--- mokepon/app.js
+++ mokepon/app.js
@@ -1,6 +1,6 @@
 document.getElementById('boton-mascota').addEventListener('click', () => {
     try {
-        alert(`Usted a seleccionado ${document.querySelector('#seleccionar-mascota input:checked').id}`)
+        alert(`Usted a seleccionado ${document.querySelector('#seleccionar-mascota input:checked').value}`)
     } catch (error) {
         alert("No ha seleccionado nada!")
     }
